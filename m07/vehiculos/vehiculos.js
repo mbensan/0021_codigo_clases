60c5990b8b72736ce2ea2befd498d8e4433bc58d
Matias Bensan Araya <mbensan@gmail.com>
2023-07-28 20:37:24 -0400

    Se completa demostración de polimorfismo y herencia

--- m07/vehiculos/vehiculos.js
+++ m07/vehiculos/vehiculos.js
@@ -10,27 +10,61 @@ Vehiculo:
 class Vehiculo {
 
   constructor(marca, modelo, motor, rendimiento) {
-    this.marca = marca
+    this._marca = marca
     this.modelo = modelo
     this.motor = motor
     this.rend = rendimiento
-    this.num_asientos // sólo aplica a los autos
-    this.tiempos // sólo aplica a las motos
   }
   gasto (kms) {
     return kms / this.rend
   }
-}
 
-class Auto extends Vehiculo {
-  constructor(marca, modelo, motor, rendimiento, num_asientos) {
-    // 1. llamamos al constructor de la clase padre
-    super(marca, modelo, motor, rendimiento)
-    // 2. iniciamos el resto de las cosas
-    this.num_asientos = num_asientos
+  get marca () {
+    return this._marca.toUpperCase()
+  }
+
+  set marca (palabra) {
+    this._marca = palabra.trim()
   }
 }
 
-const motoCarlos = new Vehiculo('Yamaha', 'Entiser', 250, 14)
-const nisan = new Vehiculo('Nissan', 'Xtrail', 2500, 10)
-const toyota = new Vehiculo('Toyota', 'Terios', 1300, 18)
+class Auto extends Vehiculo {
+
+  constructor(marca, modelo, motor, rendimiento, num_asientos) {
+    // 1. llamamos al constructor de la clase padre
+    super(marca, modelo, motor, rendimiento)
+    // 2. iniciamos el resto de las variables de instancia
+    this.num_asientos = num_asientos
+  }
+
+  gastoPorPasajero (kms) {
+    return this.gasto(kms) / this.num_asientos
+  }
+}
+class Moto extends Vehiculo {
+  constructor(marca, modelo, motor, rendimiento, tiempos) {
+    // 1. llamamos al constructor de la clase padre
+    super(marca, modelo, motor, rendimiento)
+    // 2. iniciamos el resto de las variables de instancia
+    this.tiempos = tiempos
+  }
+}
+
+class Bici extends Vehiculo {
+  
+  gasto (kms) {
+    const gasto_original = super.gasto(kms)
+    return gasto_original / 2
+  }
+}
+
+
+const motoCarlos = new Moto('Yamaha', 'Entiser', 250, 14, 4)
+const nisan = new Auto('Nissan', 'Xtrail', 2500, 10, 5)
+const toyota = new Auto('Toyota', 'Terios', 1300, 18, 4)
+
+async function init() {
+  let vehiculo;
+}
+init()
+
