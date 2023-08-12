a9962d1736bc8e0430566039b85776b57cc6f279
Matias Bensan Araya <mbensan@gmail.com>
2023-08-11 20:57:51 -0400

    Frontend reacciona a errores del servidor

--- m08/articulos/app.js
+++ m08/articulos/app.js
@@ -22,6 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'node_modules/axios/dist')));
+app.use(express.static(path.join(__dirname, 'node_modules/sweetalert2/dist')));
 
 // configuramos la subida de archivos
 app.use(fileUpload({
