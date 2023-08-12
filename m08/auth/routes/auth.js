d71a824fbee13af43a649f1cbdb0ef566da41358
Matias Bensan Araya <mbensan@gmail.com>
2023-08-12 13:01:20 -0400

    Sistema primario de autentificacion

--- m08/auth/routes/auth.js
+++ m08/auth/routes/auth.js
@@ -16,15 +16,27 @@ router.post('/login', function(req, res, next) {
   // 1. Recibo los parámetros del formulario
   const {email, password} = req.body
 
-  // 2. Calculo 1 hora más
+  // 2. Verificamos que el usuario exista en la BBDD
+  const user = users.find(obj => obj.email == email)
+  if (!user) {
+    return res.status(404).json({err: 'Usuario inexistente'})
+  }
+
+  // 3. Verificamos que la contraseña sea la correcta
+  if (user.pass != password) {
+    return res.status(400).json({err: 'Contraseña incorrecta'})
+  }
+
+  // 3. Calculo 1 hora más
   const una_hora = Math.floor(new Date()/1000) + 3600
 
-  // 3. Creo el token
+  // 4. Creo el token
   const token = jwt.sign({
     exp: una_hora,
     data: {
-      id: 5,
-      email: email
+      id: user.id,
+      email: user.email,
+      nombre: user.nombre
     }
   }, llave_secreta)
 
@@ -44,6 +56,13 @@ router.post('/read', function (req, res) {
     return res.status(400).json(error)
   }
   res.json(decoded)
+})
+
+router.get('/my', (req, res) => {
+  // quiero que esta ruta sólo sea para usuarios logueados
+  // Si puedo abrir el token, entonces asumimos que el usuario SI está logueado
+  
+  res.status(401).json({err: 'No tiene permiso para entrar'})
 })
 
 module.exports = router;
